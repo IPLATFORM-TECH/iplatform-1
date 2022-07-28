06a3de19c36f5476d00fc2555f4ee9d46659c3ea
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2022-07-28 18:44:54 +0300

    IP-45 entity\service\repo\mongo (#3)

---
+++ core/src/main/java/space/eliseev/iplatform/entity/Data.java
@@ -0,0 +1,24 @@
+package space.eliseev.iplatform.entity;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Document
+@Builder
+public class Data {
+    @Id
+    private String id;
+
+    private Object dataField;
+    private String source;
+    private int dateOfDownload;
+}
---
+++ core/src/main/java/space/eliseev/iplatform/service/DataService.java
@@ -0,0 +1,19 @@
+package space.eliseev.iplatform.service;
+
+import space.eliseev.iplatform.entity.Data;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataService {
+
+    Optional<Data> findDataBySource(String source);
+
+    List<Data> findAll();
+
+    void updateData(Data newData);
+
+    void saveData(Data data);
+
+    void deleteData(Data data);
+}
